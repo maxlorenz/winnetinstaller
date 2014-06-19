bc333caab1d5451d63ab00395df2ef6cd29629dc
maxlorenz <max-lorenz@gmx.net>
2014-06-19 16:32:41 +0200

    Webinterface improved

--- Consumer/Consumer/MainWindow.xaml.cs
+++ Consumer/Consumer/MainWindow.xaml.cs
@@ -1,18 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Consumer
 {
@@ -27,45 +15,22 @@ namespace Consumer
         public MainWindow()
         {
             InitializeComponent();
-
-            client = new WebClient();
-            client.DownloadStringCompleted += new DownloadStringCompletedEventHandler(cmdExecuted);
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            executeRequest(txtCommand.Text);
+        }
 
+        private void executeRequest(String command)
+        {
             if (lstServers.SelectedIndex == -1)
             {
                 return;
             }
 
-            executeRequest(lstServers.SelectedItem.ToString(), txtCommand.Text);
-        }
-
-        private void cmdExecuted(object sender, DownloadStringCompletedEventArgs e)
-        {
-            if (e.Error == null)
-            {
-                txtOutput.Text = e.Result;
-            }
-            else
-            {
-                txtOutput.Text = e.Error.ToString();
-            }
-        }
-
-        private void executeRequest(String server, String command)
-        {
-
-            try
-            {
-                client.DownloadStringAsync(new Uri("http://" + server + "/" + Uri.EscapeDataString(command)));
-            }
-            catch (Exception ex)
-            {
-                txtOutput.Text = ex.StackTrace;
-            }
+            string server = lstServers.SelectedItem.ToString();
+            webView.Navigate(new Uri("http://" + server + "/" + Uri.EscapeDataString(command)));
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
@@ -80,18 +45,32 @@ namespace Consumer
                 return;
             }
 
-            string cmdInfo = "wmic computersystem get domain, Manufacturer, Model, username";
-            executeRequest(lstServers.SelectedItem.ToString(), cmdInfo);
+            infoView.Navigate(new Uri("http://" + lstServers.SelectedItem + "/html/info"));
         }
 
         private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            executeRequest("cmd /c echo list disk | diskpart");
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            executeRequest("\\\\wds01noham.bo.nexinto.com\\osdeploy\\webinterface\\install.bat");
+        }
+
+        private void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+            executeRequest("wpeutil reboot");
+        }
+
+        private void Button_Click_6(object sender, RoutedEventArgs e)
         {
             if (lstServers.SelectedIndex == -1)
             {
                 return;
             }
 
-            executeRequest(lstServers.SelectedItem.ToString(), "test.bat");
+            infoView.Navigate(new Uri("http://" + lstServers.SelectedItem + "/plain/info"));
         }
     }
 }
--- Webinterface/src/experiment.go
+++ Webinterface/src/experiment.go
@@ -6,31 +6,108 @@ import (
 	"net/http"
 	"os"
 	"os/exec"
+	"strconv"
 	"strings"
 )
+
+var htmlHeader string = `
+<html><head><meta http-equiv="content-type" content="text/html;charset=utf-8">
+<meta http-equiv="refresh" content="3" ><style type="text/css">html{
+	font-family: "Consolas";font-size: 12;background-color: black;color: white;white-space: pre;
+	margin: 1em;}a:link, a:visited, a:hover, a:active{color: orange;}.green{color: green;}
+	.orange{color: orange;}.red{color: red;}input{width: 80%;}</style></head><body>
+	<br><br>
+`
+
+type command struct {
+	Command, Result string
+	FullCommand     []string
+	Running, Error  bool
+}
+
+var commands []command = make([]command, 0)
 
 func exitHandler(w http.ResponseWriter, r *http.Request) {
 	os.Exit(0)
 }
 
+func htmlInfoHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, htmlHeader)
+	for i := len(commands) - 1; i >= 0; i-- {
+		command := commands[i]
+		fmt.Fprintf(w, "<p href=\"/test\" class=\"")
+		if command.Running {
+			fmt.Fprintf(w, "orange")
+		} else if command.Error {
+			fmt.Fprintf(w, "red")
+		} else {
+			fmt.Fprintf(w, "green")
+		}
+		fmt.Fprintf(w, "\">"+command.Command+"</p>")
+		fmt.Fprintf(w, "<small>"+strings.Join(command.FullCommand, " "))
+		fmt.Fprintf(w, " | <a href=\"/html/result/"+strconv.Itoa(i)+"\">Result</a></small><br><br>")
+	}
+}
+
+func plainInfoHandler(w http.ResponseWriter, r *http.Request) {
+	for i := len(commands) - 1; i >= 0; i-- {
+		command := commands[i]
+		if command.Running {
+			fmt.Fprintf(w, "Running: ")
+		} else if command.Error {
+			fmt.Fprintf(w, "Error: ")
+		} else {
+			fmt.Fprintf(w, "Successful (" + strconv.Itoa(i) + "): ")
+		}
+		fmt.Fprintf(w, command.Command + "\n")
+		fmt.Fprintf(w, "%s\n\n", command.FullCommand)
+	}
+}
+
+func htmlResultHandler(w http.ResponseWriter, r *http.Request) {
+	commandNumber, _ := strconv.Atoi(r.URL.Path[len("/html/result/"):])
+	if commandNumber < len(commands) && commandNumber >= 0 {
+		fmt.Fprintf(w, htmlHeader)
+		fmt.Fprintf(w, commands[commandNumber].Result)
+	}
+}
+
+func plainResultHandler(w http.ResponseWriter, r *http.Request) {
+	commandNumber, _ := strconv.Atoi(r.URL.Path[len("/plain/result/"):])
+	if commandNumber < len(commands) && commandNumber >= 0 {
+		fmt.Fprintf(w, commands[commandNumber].Result)
+	}
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
 	args := strings.Split(r.URL.Path[1:], " ")
+
+	place := len(commands)
+	commands = append(commands, command{Command: args[0], FullCommand: args, Running: true})
 
 	// args[0] is the command, args[1:] are the options.
 	// e.g. ping www.google.de : args = ["ping", "www.google.de"]
 	out, err := exec.Command(args[0], args[1:]...).Output()
 
 	if err != nil {
-		fmt.Fprintf(w, "Command did not execute correctly")
+		commands[place].Result = "Command did not execute correctly"
+		commands[place].Error = true
 	} else {
-		fmt.Fprintf(w, "%s", string(out))
+		commands[place].Result = string(out)
 	}
+
+	commands[place].Running = false
+	fmt.Fprintf(w, commands[place].Result)
 }
 
 func main() {
 	fmt.Println(net.InterfaceAddrs())
 
-	http.HandleFunc("/exit", exitHandler)
+	http.HandleFunc("/exit/", exitHandler)
+	http.HandleFunc("/html/info/", htmlInfoHandler)
+	http.HandleFunc("/plain/info/", plainInfoHandler)
+	http.HandleFunc("/html/result/", htmlResultHandler)
+	http.HandleFunc("/plain/result/", plainResultHandler)
 	http.HandleFunc("/", handler)
 	http.ListenAndServe(":12345", nil)
 
